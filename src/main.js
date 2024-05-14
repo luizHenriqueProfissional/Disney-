59c52d4e2e144c33b83db72a9fed05986271fcf6
Luiz <luiz.3352@gmail.com>
2024-05-14 08:42:04 -0300

    inclusão js

---
+++ src/main.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoad', function(){
+    const buttons = document.querySelectorAll('[data-tab-button]');
+
+    for (let  i = 0;  i < buttons.length; i++) {
+        buttons[i].addEventListener('click', function(botao){
+            console.log(botao);
+        })
+    }
+})
